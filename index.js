3092acbe868128daaf920969c880556c5b7b059e
DarkHole1 <dark.hole1@yandex.ru>
2020-07-12 15:25:27 +0300

    Debug

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 try {
   const token = core.getInput('access-token');
   const octokit = github.getOctokit(token);
+  console.log(octokit);
   octokit.checks.create({
     ...github.context.repo,
     head_sha: github.context.sha,
